a0e0e16348ec84cc02ed566c0ea4dc86ab595c74
carlos1377 <carloseduardoorso1@gmail.com>
2024-04-11 22:49:21 -0300

    created flowviewset and updated urls

--- vrenda_api/api/urls.py
+++ vrenda_api/api/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from api import views
+from rest_framework import routers
 
-urlpatterns = [
-    path('health-check/', views.health_check)
-]
+router = routers.DefaultRouter()
+
+router.register(r'flows', views.FlowsViewSet)
+# router.register(r'health-check', views.health_check)
+
+# urlpatterns = [
+#     path('health-check/', views.health_check)
+# ]
+
+urlpatterns = router.urls
+
+urlpatterns.append(path('health-check/', views.health_check))
--- vrenda_api/api/views.py
+++ vrenda_api/api/views.py
@@ -1,8 +1,15 @@
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Flow
+from .serializer import FlowSerializer
 
 
 @api_view()
 def health_check(request):
     return Response({'message': 'OK'})
+
+
+class FlowsViewSet(viewsets.ModelViewSet):
+    queryset = Flow.objects.all()
+    serializer_class = FlowSerializer
